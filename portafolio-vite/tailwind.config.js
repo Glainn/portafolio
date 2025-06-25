0e704ed12c50632efe9ee206755948546986bf87
Glainn <glainnelycpb@gmail.com>
2025-06-24 21:53:17 -0400

    Después de unas horas eligiendo los colores

---
+++ portafolio-vite/tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        'serif': ['"Cormorant Garamond"', 'serif'], // Para títulos
+        'sans': ['"Source Sans 3"', 'sans-serif'], // Para el cuerpo del texto
+      },
+    },
+  },
+  plugins: [],
+}
